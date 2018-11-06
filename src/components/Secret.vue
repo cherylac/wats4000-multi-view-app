65da9dbc9ef1772df1dfa7e98032d5d6ff5d1943
cherylac <38063622+cherylac@users.noreply.github.com>
2018-11-05 23:33:26 -0800

    finished part 2

---
+++ src/components/Secret.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="component">
+    <h2>{{ message }}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        message: 'Mind your syntax and keep your code clean!'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .component {
+    background: #e8e8e8;
+    min-height: 400px;
+  }
+</style>
